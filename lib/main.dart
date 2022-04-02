83a59731ef7d592e2148be9e554372d5c497392e
Ja-e <leeniesta@naver.com>
2022-04-02 17:23:46 +0900

    주사위 게임 첫 커밋

---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Dice game',
+      home: LogIn(),
+    );
+  }
+}
+
+class LogIn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Log in'),
+        backgroundColor: Colors.redAccent,
+        centerTitle: true,
+        leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: (){}
+        ),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: (){}
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(padding: EdgeInsets.only(top: 50)),
+            Center(
+              child:
+              Image(image: AssetImage('image/chef.gif'),
+                width: 170,
+                height: 190,
+              ),
+            ),
+            Form(
+                child: Theme(
+                  data: ThemeData(
+                    primaryColor: Colors.teal,
+                    inputDecorationTheme: InputDecorationTheme(
+                      labelStyle: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 15
+                      )
+                    )
+                  ),
+                child: Container(
+                  padding: EdgeInsets.all(40),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Enter "Dice"'
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: 'Enter Password'
+                        ),
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                      ),
+                      SizedBox(height: 40,),
+                      ButtonTheme(
+                          minWidth: 100,
+                            height: 50,
+                            child: RaisedButton(
+                                color: Colors.orangeAccent,
+                                  child: Icon(
+                                      Icons.arrow_forward,
+                                    color: Colors.white,
+                                    size: 35,
+                                  ),onPressed: null
+                            )),
+                    ],
+                  ),
+                ),))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
